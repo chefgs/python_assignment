5f9cde0da79094653574c91ada60bffdb069e972
Saravanan Gnanaguru <g.gsaravanan@gmail.com>
2024-02-28 14:37:46 +0530

    Removed files

--- print_user_text.py
+++
@@ -1,5 +0,0 @@
-# Prompt the user to enter their custom text
-user_input_text = input("Enter your text to print: ")
-
-# Print the custom text entered by the user
-print("You entered:", user_input_text)
