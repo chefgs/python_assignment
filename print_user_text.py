a923d169a282a6170879edaf5f7e251777931d5a
Saravanan G <g.gsaravanan@gmail.com>
2024-02-28 14:31:49 +0530

    Initial commit

---
+++ print_user_text.py
@@ -0,0 +1,5 @@
+# Prompt the user to enter their custom text
+user_input_text = input("Enter your text to print: ")
+
+# Print the custom text entered by the user
+print("You entered:", user_input_text)
